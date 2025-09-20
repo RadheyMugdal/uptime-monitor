1f91be16e6d2d68600c782e88d760fbc12e4899a
Radhey Mugdal <mugdalradhey@gmail.com>
2025-09-20 16:30:33 +0530

    fix: Resolve build errors

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,10 +1,12 @@
 import { LoginForm } from '@/modules/auth/ui/components/login-form'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const SignInPage = () => {
     return (
         <div className='w-screen h-screen'>
+            <Suspense fallback={<div>Loading...</div>}>
             <LoginForm />
+            </Suspense>
         </div>
     )
 }
