a8fa19ed128e33663a4f7bf147de99cbcd74de73
Radhey Mugdal <mugdalradhey@gmail.com>
2025-12-13 20:31:07 +0530

    refactor: use Next base url from env in base url of authclient

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -5,5 +5,5 @@ export const authClient = createAuthClient({
         polarClient()
     ],
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL
 })
